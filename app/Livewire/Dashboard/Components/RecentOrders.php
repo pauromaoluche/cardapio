858fbf449127979f0624f3452fcd2ed819ddd852
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2025-08-08 15:01:27 -0300

    pedidos recentes

---
+++ app/Livewire/Dashboard/Components/RecentOrders.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Components;
+
+use Livewire\Component;
+
+class RecentOrders extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.components.recent-orders');
+    }
+}
---
+++ resources/views/livewire/dashboard/components/recent-orders.blade.php
@@ -0,0 +1,3 @@
+<div class="row g-4">
+    <x-recent-order-card />
+</div>
