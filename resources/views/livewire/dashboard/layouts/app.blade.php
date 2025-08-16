5aa24363e96376aaa2be160ca21e036c2e04a7de
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2025-08-16 09:13:17 -0300

    ajuste no nome

--- resources/views/livewire/dashboard/layouts/app.blade.php
+++ resources/views/livewire/dashboard/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ $title ?? 'FoodAdmin - Painel de Administração' }}</title>
+    <title>{{ $title ?? 'Ps - Cardapio - Painel de Administração' }}</title>
 
     <meta name="description"
         content="Sistema de gerenciamento para lanchonetes - Controle de pedidos, clientes e relatórios">
