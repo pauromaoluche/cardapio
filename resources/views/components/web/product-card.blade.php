7584cacba8dfdcd391497a1a44dfae936462a015
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2025-08-21 10:42:24 -0300

    Centralizando component de product-cart

--- resources/views/components/web/product-card.blade.php
+++ resources/views/components/web/product-card.blade.php
@@ -1,47 +1,84 @@
 @props(['item' => []])
 
 <div class="product-card" wire:ignore.self>
-    <div class="row g-0 p-3">
+    <div class="row g-0 p-3 promotion-row">
         <div class="col-auto me-3">
             @if (!empty($item['images']))
-                <img src="{{ asset('storage/' . $item['images'][0]['path']) }}" alt="{{ $item['name'] }}"
-                    class="product-image">
+                <img src="{{ asset('storage/' . $item['images'][0]['path']) }}"
+                    alt="{{ $item['name'] ?? $item['title'] }}" class="product-image">
             @endif
         </div>
         <div class="col">
             <div class="product-info">
                 <h5 class="product-name">
-                    {{ $item['name'] }}
-                    <!-- VERIFICAÇÃO DE DESCONTO -->
-                    @if (!empty($item['discount']))
+                    {{ $item['name'] ?? $item['title'] }}
+                    @if ($item['type'] == 'product' && !empty($item['discount']))
                         <span class="badge bg-success ms-2">Desconto</span>
+                    @elseif ($item['type'] === 'promotion')
+                        <span class="badge promotion ms-2">Promoção</span>
                     @endif
                 </h5>
-                <p class="product-description">{{ $item['description'] }}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <!-- EXIBIÇÃO DE PREÇO COM DESCONTO -->
-                    @if (!empty($item['discount']))
-                        @php
-                            $finalPrice = $item['price'];
-                            if ($item['discount']['discount_type'] === 'percentage') {
-                                $finalPrice = $finalPrice - $finalPrice * ($item['discount']['discount_value'] / 100);
-                            } elseif ($item['discount']['discount_type'] === 'fixed') {
-                                $finalPrice = $finalPrice - $item['discount']['discount_value'];
-                            }
-                        @endphp
-                        <div class="d-flex flex-column">
-                            <small class="text-muted text-decoration-line-through">R$
-                                {{ number_format($item['price'], 2, ',', '.') }}</small>
-                            <span class="product-price text-success fw-bold">R$
-                                {{ number_format($finalPrice, 2, ',', '.') }}</span>
+                @if ($item['type'] === 'promotion')
+                    <p class="promotion-description">{{ $item['description'] }}</p>
+
+                    <div class="promoted-products mt-2">
+                        <small class="text-secondary">Itens na promoção:</small>
+                        <ul>
+                            @foreach ($item['products'] as $promotedProduct)
+                                <li><small>x{{ $promotedProduct['pivot']['quantity'] }}
+                                        {{ $promotedProduct['name'] }}</small></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @else
+                    <p class="product-description">{{ $item['description'] }}</p>
+                @endif
+
+                <div class="row">
+                    @if (!empty($item['discount']) || $item['type'] === 'promotion')
+                        <div class="col-12 prices-container mt-2 d-flex align-items-center justify-content-between">
+                            <div>
+                                <small class="text-muted text-decoration-line-through me-2">R$
+                                    {{ number_format($item['price'], 2, ',', '.') }}</small>
+                                <p class="promotion-price text-success fw-bold mt-1 mb-0">R$
+                                    {{ number_format($item['final_price'], 2, ',', '.') }}</p>
+                            </div>
+                            @if (!empty($item['discount']))
+                                <button type="button" class="add-btn"
+                                    wire:click="selectProduct('{{ $item['key'] }}')">
+                                    <i class="fas fa-plus me-1"></i>Adicionar
+                                </button>
+                            @endif
                         </div>
+                        @if ($item['type'] === 'promotion')
+                            <div class="col-12 d-flex justify-content-between align-items-center mt-1">
+                                <span class="discount-info">
+                                    @if ($item['discount_type'] === 'percentage')
+                                        <small class="text-secondary">Desconto de
+                                            {{ number_format($item['discount_value'], 1, ',') }}%</small>
+                                    @elseif ($item['discount_type'] === 'fixed')
+                                        <small class="text-secondary">Desconto de R$
+                                            {{ number_format($item['discount_value'], 2, ',', '.') }}
+                                            OFF</small>
+                                    @endif
+                                </span>
+
+                                <button type="button" class="add-btn"
+                                    wire:click="selectPromotion('{{ $item['key'] }}')">
+                                    <i class="fas fa-plus me-1"></i>Adicionar
+                                </button>
+                            </div>
+                        @else
+                        @endif
                     @else
-                        <span class="product-price">R$
-                            {{ number_format($item['price'], 2, ',', '.') }}</span>
+                        <div class="col-12 d-flex justify-content-between align-items-center mt-1">
+                            <span class="product-price">R$
+                                {{ number_format($item['price'], 2, ',', '.') }}</span>
+                            <button type="button" class="add-btn" wire:click="selectProduct('{{ $item['key'] }}')">
+                                Adicionar
+                            </button>
+                        </div>
                     @endif
-                    <button type="button" class="add-btn" wire:click="selectProduct('{{ $item['key'] }}')">
-                        Adicionar
-                    </button>
                 </div>
             </div>
         </div>
--- resources/views/livewire/web/pages/home/index.blade.php
+++ resources/views/livewire/web/pages/home/index.blade.php
@@ -2,13 +2,8 @@
     <livewire:web.components.categories />
     <div class="container-custom">
         <div class="menu-section">
-            <!-- Loop para produtos e promoções -->
             @foreach ($products as $item)
-                @if ($item['type'] === 'product')
-                    <x-web.product-card :item="$item" />
-                @elseif ($item['type'] === 'promotion')
-                    <x-web.promotion-card :item="$item" />
-                @endif
+                <x-web.product-card :item="$item" />
             @endforeach
         </div>
     </div>
